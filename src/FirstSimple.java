99f22924d192fdb4e595dd9b40e11573be1bb25a
Edward Gavin <wsw_it_3325@163.com>
2020-02-02 10:34:30 +0800

    hello review Java

---
+++ src/FirstSimple.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: Edward Gavin
+ * @Create: 2020-02-02 10:20
+ */
+public class FirstSimple {
+    public static void main(String[] args) {
+        System.out.println("we will not use Hello world! ");
+    }
+}
